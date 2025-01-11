a5df4934a8c9d1a7729d9ad92ae7df688a4c840b
DIEGO FERNANDO YAMA ANDRADE <diegoyamaa@gmail.com>
2025-01-11 11:00:28 -0500

    creacion de servicio para consumir el procedimiento almacenado

---
+++ Services/PersonaService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prueba_YamaAndrade.Data;
+
+namespace Prueba_YamaAndrade.Services
+{
+    public class PersonaService
+    {
+        private readonly AppDBContext _appDbContext;
+
+        public PersonaService(AppDBContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void CrearTablaPersona(string nuevoNombreTabla)
+        {
+            var sql = "EXEC CrearTablaPersona @nuevoNombreTabla";
+
+            // Ejecutar el procedimiento almacenado
+            _appDbContext.Database.ExecuteSqlRaw(sql, new { nuevoNombreTabla });
+        }
+    }
+}
